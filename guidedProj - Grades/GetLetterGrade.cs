5db24355b86a9632002e734ed477d317791e6409
Hanson <Mhanson@wgeld.org>
2023-12-14 15:30:30 -0500

    first commit

---
+++ guidedProj - Grades/GetLetterGrade.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guidedProj___Grades
+{
+    public class GetLetterGrade
+    {
+        public static string getLetter(decimal numGrade = 0)
+        {
+            string ltrGrade = "F";
+            if (numGrade >= 97)
+                ltrGrade = "A+";
+            else if (numGrade >= 93)
+                ltrGrade = "A";
+            else if (numGrade >= 90)
+                ltrGrade = "A-";
+            else if (numGrade >= 87)
+                ltrGrade = "B+";
+            else if (numGrade >= 83)
+                ltrGrade = "B";
+            else if (numGrade >= 80)
+                ltrGrade = "B-";
+            else if (numGrade >= 77)
+                ltrGrade = "C+";
+            else if (numGrade >= 73)
+                ltrGrade = "C";
+            else if (numGrade >= 70)
+                ltrGrade = "C-";
+            else if (numGrade >= 67)
+                ltrGrade = "D+";
+            else if (numGrade >= 63)
+                ltrGrade = "D";
+            else if (numGrade >= 60)
+                ltrGrade = "D-";
+            else
+                ltrGrade = "F";
+
+            return ltrGrade;
+        }
+    }
+}
---
+++ guidedProj - Grades/Program.cs
@@ -0,0 +1,50 @@
+﻿using guidedProj___Grades;
+using System;
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+string[] studentNames = new string[] { "Logan", "Sophia", "Emma", "Andrew" };
+int[] studentScores = new int[10];
+string studentLetterGrade = "";
+
+int[] loganScores = new int[] {90,95,87,88,96};
+int[] emmaScores = new int[] {90,85,87,98,68 };
+int[] andrewScores = new int[] {92,89,81,96,90};
+int[] sophiaScores = new int[] {90,86,87,98,100};
+
+
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+    else studentScores = emmaScores;
+
+    int sumScores = 0;
+    decimal currStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        sumScores += score;
+    }
+
+
+    currStudentGrade = (decimal)sumScores / currentAssignments;
+
+
+    studentLetterGrade = GetLetterGrade.getLetter(currStudentGrade);
+
+
+    Console.WriteLine($"{currentStudent}\t\t{currStudentGrade}\t???");
+
+}
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
